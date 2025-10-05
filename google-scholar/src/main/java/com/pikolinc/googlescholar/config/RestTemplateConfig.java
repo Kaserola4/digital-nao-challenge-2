501084e76f10471782c2e9b9fea0ac7c39ff1483
Dani <79950554+Kaserola4@users.noreply.github.com>
2025-10-04 21:22:21 -0600

    refactor(restTemplate): moved to config package

---
+++ google-scholar/src/main/java/com/pikolinc/googlescholar/config/RestTemplateConfig.java
@@ -0,0 +1,13 @@
+package com.pikolinc.googlescholar.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+class RestTemplateConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
