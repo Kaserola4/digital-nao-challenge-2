52e8a3ae969762262b408d77f868d16e253e90f5
Dani <79950554+Kaserola4@users.noreply.github.com>
2025-10-04 19:12:09 -0600

    feat(exception handling): Now it can handle all exceptions with a default response

--- google-scholar/src/main/java/com/pikolinc/googlescholar/exception/GlobalExceptionHandlerAdvice.java
+++ google-scholar/src/main/java/com/pikolinc/googlescholar/exception/GlobalExceptionHandlerAdvice.java
@@ -19,14 +19,24 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  */
 @RestControllerAdvice
 class GlobalExceptionHandlerAdvice {
+
     public record ErrorResponse(int status, String message) { }
 
     @ExceptionHandler(ResponseException.class)
     public ResponseEntity<ErrorResponse> handleResponseException(ResponseException exception) {
-
         HttpStatus httpStatus = exception.getHttpStatus();
         ErrorResponse errorResponse = new ErrorResponse(httpStatus.value(), exception.getMessage());
 
         return ResponseEntity.status(httpStatus).body(errorResponse);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGenericException(Exception exception) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "An unexpected error occurred. Please try again later."
+        );
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
     }
 }
